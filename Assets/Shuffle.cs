95f6eaf4730c3f1275a6b35e31d6961dfee405db
Han Jung <gamedevhan@gmail.com>
2018-07-15 19:22:41 -0400

    Unity implementation of Fisher-Yate's shuffle algorithm

---
+++ Assets/Shuffle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shuffle : MonoBehaviour
+{
+	public List<Transform> Cubes = new List<Transform>();	
+		
+	public void OnButtonPress()
+	{		
+		ShuffleList(Cubes);
+		Redraw();
+	}
+
+	private void ShuffleList<T>(IList<T> list)
+	{
+		for (int i = list.Count - 1; i > 0; i--)
+		{
+			int random = Random.Range(0, list.Count);
+			T value = list[random];
+			list[random] = list[i];
+			list[i] = value;
+		}
+	}
+
+	private void Redraw()
+	{
+		for (int i = 0; i < Cubes.Count; i++)
+		{	
+			Cubes[i].position = new Vector3( (i * 2) - 4, 0, 0);
+		}
+	}
+}
